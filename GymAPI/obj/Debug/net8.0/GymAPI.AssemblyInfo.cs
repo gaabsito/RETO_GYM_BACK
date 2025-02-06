ab2a10c11e7b9cc99c0efb3551e3544ee29e34f5
gaabsito <157628366+gaabsito@users.noreply.github.com>
2025-02-06 01:38:09 +0100

    gitignore

--- GymAPI/obj/Debug/net8.0/GymAPI.AssemblyInfo.cs
+++ GymAPI/obj/Debug/net8.0/GymAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GymAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65cd0bc87f5f268ecf01a6feeca4db7060c6aff5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bd99bf0c35248fb662acacdd01166177bfc3d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("GymAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GymAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
