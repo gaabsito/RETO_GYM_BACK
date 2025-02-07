8d5e938cebd340f915a40108eacdffa898bd1244
gaabsito <157628366+gaabsito@users.noreply.github.com>
2025-02-07 19:30:51 +0100

    TERMINADO EJERCICIO

--- GymAPI/Program.cs
+++ GymAPI/Program.cs
@@ -15,14 +15,15 @@ builder.Services.AddScoped<IEntrenamientoRepository>(provider =>
     new EntrenamientoRepository(connectionString));
 
 
-
+builder.Services.AddScoped<IEjercicioRepository>(provider =>
+    new EjercicioRepository(connectionString));
 
 // Registrar los servicios
 builder.Services.AddScoped<IUsuarioService, UsuarioService>(); // ✅ CORRECTO
 
 builder.Services.AddScoped<IEntrenamientoService, EntrenamientoService>(); // ✅ CORRECTO
 
-
+builder.Services.AddScoped<IEjercicioService, EjercicioService>(); // ✅ CORRECTO
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
