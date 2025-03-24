48c0c2e2e36d252b4d9ef9e09cfe7f3fec6bbc56
gaabsito <157628366+gaabsito@users.noreply.github.com>
2025-03-24 17:35:14 +0100

    Update Usuario.cs

--- GymAPI/Models/Usuario.cs
+++ GymAPI/Models/Usuario.cs
@@ -8,9 +8,15 @@ namespace GymAPI.Models
         public string Nombre { get; set; } = "";
         public string Apellido { get; set; } = "";
         public string Email { get; set; } = "";
-        public int? Edad { get; set; }   // Nullable
-        public float? Peso { get; set; } // Nullable
-        public float? Altura { get; set; } // Nullable
+        public int? Edad { get; set; }
+        public float? Peso { get; set; }
+        public float? Altura { get; set; }
+        
+        // Nuevos campos para objetivos
+        public float? ObjetivoPeso { get; set; }
+        public float? ObjetivoIMC { get; set; }
+        public float? ObjetivoPorcentajeGrasa { get; set; }
+        
         public DateTime FechaRegistro { get; set; } = DateTime.Now;
         public bool EstaActivo { get; set; } = true;
 
